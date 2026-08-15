2cec94b1c4dbc9a2434b5c208cbe9da3323f6321
Sushil Bhattarai <bhattaraisushil371@gmail.com>
2026-08-15 13:35:17 +0545

    changed small

--- socket-server/src/server.ts
+++ socket-server/src/server.ts
@@ -1,6 +1,5 @@
 import "dotenv/config";
 import http from "http";
-
 import app from "./app";
 import { connectDatabase } from "./config/db";
 import { initializeSocket } from "./socket/socket";
@@ -9,28 +8,20 @@ const PORT = Number(process.env.PORT) || 8008;
 
 const startServer = async () => {
   try {
-    
-     //  Connect database
     await connectDatabase();
 
-    
-    // Create HTTP server
     const httpServer = http.createServer(app);
 
-    
-    // Initialize Socket.IO
     initializeSocket(httpServer);
 
-    // Start server
-    
-    httpServer.listen(PORT, () => {
+    httpServer.listen(PORT, "0.0.0.0", () => {
       console.log(`V-EGAL Socket Server running on port ${PORT}`);
       console.log(`Health: http://localhost:${PORT}/health`);
     });
   } catch (error) {
-    console.error("Failed to start V-EGAL Socket Server:", error );
+    console.error("Failed to start V-EGAL Socket Server:", error);
     process.exit(1);
   }
 };
 
-startServer();
+startServer();  
